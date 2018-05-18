72e93063dab644bdc4a32969b1a31cd866b7dd11
J.-Fabian Wenisch <privat@jfwenisch.com>
2018-05-18 16:27:51 +0200

    - Refactoring Project Structure moved Object Classes ( User, Credentials ) to datastructure project so it can be reused by the different parent projects

---
+++ src/main/java/de/madana/security/DC_HashHandler.java
@@ -0,0 +1,58 @@
+package de.madana.security;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class DC_HashHandler 
+{
+	
+	   public static byte[] createChecksum(String filename) throws Exception {
+	       InputStream fis =  new FileInputStream(filename);
+
+	       byte[] buffer = new byte[1024];
+	       MessageDigest complete = MessageDigest.getInstance("MD5");
+	       int numRead;
+
+	       do {
+	           numRead = fis.read(buffer);
+	           if (numRead > 0) {
+	               complete.update(buffer, 0, numRead);
+	           }
+	       } while (numRead != -1);
+
+	       fis.close();
+	       return complete.digest();
+	   }
+
+	   // see this How-to for a faster way to convert
+	   // a byte array to a HEX string
+	   public static String getMD5Checksum(String filename) throws Exception {
+	       byte[] b = createChecksum(filename);
+	       String result = "";
+
+	       for (int i=0; i < b.length; i++) {
+	           result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+	       }
+	       return result;
+	   }
+
+	public static String generateHash(String plaintext) throws NoSuchAlgorithmException
+	{
+		MessageDigest m = MessageDigest.getInstance("SHA-256");
+		m.reset();
+		m.update(plaintext.getBytes());
+		byte[] digest = m.digest();
+		BigInteger bigInt = new BigInteger(1,digest);
+		String hashtext = bigInt.toString(16);
+		// Now we need to zero pad it if you actually want the full 32 chars.
+		while(hashtext.length() < 32 )
+		{
+		  hashtext = "8"+hashtext;
+		}
+		return hashtext;
+	}
+
+}
